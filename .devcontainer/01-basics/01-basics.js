dc20ab98114e4a9913bda0402b8777968ae97f84
Hijab Nawaz <hijabnawaz073@gmail.com>
2024-08-12 11:13:04 +0000

    my first commit

---
+++ .devcontainer/01-basics/01-basics.js
@@ -0,0 +1,13 @@
+const accountId= 1234
+let Email= "hijabnawaz073@gamil.com"
+var accpassword= "78900"
+accountCity= "islamabad"
+// accountId=2 // not allowed 
+ Email="hiba@gamil.com"
+ accpassword="4567"
+ accountCity="lahore"
+
+//  prefer not to use var because of block scope
+
+console.log(accountId);
+console.table([Email, accpassword, accountCity])
---
+++ .devcontainer/01-basics/03-conversion.js
@@ -0,0 +1,44 @@
+// let age="hiba"
+
+// console.log(typeof age);
+// console.log(typeof age);
+
+// let valueInnum= Number(age)
+// console.log(typeof valueInnum);
+// console.log(valueInnum);
+
+
+// 33 => 33 num
+// "33abc" => Nan
+// true => boolen
+
+// let loggedIn=""
+// let boolenloggedIn= Boolean(loggedIn)
+// console.log(boolenloggedIn)
+
+// let someNumber=89
+// let stringNumber= String(someNumber)
+// console.log(stringNumber)
+// console.log( typeof stringNumber)
+
+/// opraetions
+let value= 3
+let negValue= -value
+console.log(negValue)
+
+// let str1="hiba"
+// let str2=" hijab"
+
+// let str3= str1 + str2
+// console.log(str3)
+
+// console.log("1"+2+2)
+// console.log(1+"2"+"2")
+// console.log(+true)
+
+let num1, num2, num3
+num1=num2= num3= 2+2
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
